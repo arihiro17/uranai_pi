e22ff6c6e440c4d5241cda6dea531d53ba873e66
arihiro17 <arihiro@ku-ma.jp>
2019-11-15 02:41:33 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,124 @@
+from janome.tokenizer import Tokenizer
+import json
+import re
+import requests
+
+# テキストファイルを読み込む
+sjis = open("lyrics.txt", "rb").read()
+text = sjis.decode("utf_8")
+
+# 形態素解析
+t = Tokenizer()
+words = t.tokenize(text)
+
+# 辞書を作成
+def make_dic(words):
+    tmp = ["@"]
+    dic = {}
+    for i in words:
+        word = i.surface
+        if word == "" or word == "\r\n" or word == "\n": continue
+        tmp.append(word)
+        if len(tmp) < 3: continue
+        if len(tmp) > 3: tmp = tmp[1:]
+        set_word3(dic, tmp)
+        if word == "。":
+            tmp = ["@"]
+            continue
+    return dic
+
+# 三要素のリストを辞書として登録
+def set_word3(dic, s3):
+    w1, w2, w3 = s3
+    if not w1 in dic: dic[w1] = {}
+    if not w2 in dic[w1]: dic[w1][w2] = {}
+    if not w3 in dic[w1][w2]: dic[w1][w2][w3] = 0
+    dic[w1][w2][w3] += 1
+
+dic = make_dic(words)
+json.dump(dic, open("markov-blob.json", "w", encoding="utf-8"))
+
+# 自動生成
+dic = open("markov-blob.json", "r")
+dic = json.load(dic)
+
+tweets_list = []
+import random
+def word_choice(sel):
+    keys = sel.keys()
+    ran = random.choice(list(keys))
+    return ran
+
+def make_sentence(dic):
+    ret = []
+    if not "@" in dic: return "no dic"
+    top = dic["@"]
+    w1 = word_choice(top)
+    w2 = word_choice(top[w1])
+    ret.append(w1)
+    ret.append(w2)
+    for i in range(32):
+        w3 = word_choice(dic[w1][w2])
+        ret.append(w3)
+        if w3 == "\n": break
+        w1, w2 = w2, w3
+    tweets_list.append(ret)
+    return "".join(ret)
+
+def check(sentence):
+    # print(sentence)
+    # checkText = t.tokenize(sentence, stream=True)
+
+    arr = re.split('[ 　]', sentence)
+    # print(arr)
+    arr[len(arr) - 1] = ''
+    return "".join(arr)
+
+    # for word in checkText:
+    #     print(word)
+
+
+# for i in range(1):
+#     s = make_sentence(dic)
+#     s = check(s)
+#     # tweets_list.append(s)
+#     print(s)
+
+
+class GroveGSRSensor:
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+    
+    @property
+    def GSR(self):
+        value = self.adc.read(self.channel)
+        return value
+
+Grove = GroveGSRSensor
+
+api = "http://127.0.0.1:3000/api/generate/"
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: {} adc_channel".format(sys.argv[0]))
+        sys.exit(1)
+    
+    sensor = GroveGSRSensor(int(sys.argv[1]))
+
+    print("Detecting...")
+    while True:
+        print("GSR value: {0}".format(sensor.GSR))
+        if 300 > sensor.GSR:
+            s = make_sentence(dic)
+            s = check(s)
+            response = requests.post(
+                "http://127.0.0.1:3000/api/generate/",
+                {'text': s}
+            )
+            time.sleep(30)
+
+        time.sleep(.3)
+
+if __name__ == "__main__":
+    main()
---
+++ receitGenerator.js
@@ -0,0 +1,159 @@
+import path from 'path';
+import fs from 'fs';
+import　{ createCanvas, loadImage } from 'canvas';
+import SerialPort from 'serialport';
+import express from 'express';
+import multer from 'multer';
+
+
+const RECEIPT_WIDTH = 384;
+
+const BMP_BYTES_PER_LINE = RECEIPT_WIDTH / 8;
+
+const DEFAULT_TIMEOUT = 10000;
+
+const MAX_USBFS_BUFFER_SIZE = 16384;
+
+const MESSAGE_ARRAY = [
+    "そのままで大丈夫！",
+    "携帯落とさないでね",
+    "体には気をつけてね",
+    "財布忘れないでね",
+    "月曜日気をつけてね",
+    "火曜日気をつけてね",
+    "水曜日気をつけてね",
+    "木曜日気をつけてね",
+    "金曜日気をつけてね",
+];
+
+const printer = new SerialPort('/dev/ttys0', {
+    baudRate: 38400,
+    dataBits: 8,
+});
+
+
+class ReceiptGenerater {
+    generateImage(text) {
+        return new Promise((resolve, reject) => {
+            Promise.all([
+                loadImage(path.join(__dirname, './receiptParts/logo.png')),
+                loadImage(path.join(__dirname, './receiptParts/heading.png')),
+            ]).then((images) => {
+
+                const calcCanvas = createCanvas(0, 0);
+                const calcCtx = calcCanvas.getContext('2d');
+                calcCtx.fillStyle = '#000';
+                calcCtx.strokeStyle = '#000';
+                calcCtx.lineWidth = 1;
+
+                // キャンバスサイズ割り出し
+                let size = { x: 0, y: 0 };
+                this.drawLogo(calcCtx, images[0], size, false);
+                this.drawResultText(calcCtx, text, size, false);
+                this.drawHeading(calcCtx, images[1], size, false);
+                this.drawMessage(calcCtx, size, false);
+
+                const renderCanvas = createCanvas(0, 0);
+                const renderCtx = renderCanvas.getContext('2d');
+                renderCtx.fillStyle = '#000';
+                renderCtx.strokeStyle = '#000';
+                renderCtx.lineWidth = 1;
+
+                let offset = { x: 0, y: 0 };
+                this.drawLogo(calcCtx, images[0], offset, false);
+                this.drawResultText(calcCtx, text, offset, false);
+                this.drawHeading(calcCtx, images[1], offset, false);
+                this.drawMessage(calcCtx, offset, false);
+
+                let header = new Buffer.from([ parseInt('0x1c', 16), parseInt('0x2a', 16), parseInt('0x65', 16) ]);
+                let color = renderCtx.getImageData(0, 0, RECEIPT_WIDTH, offset.y);
+                let mono = [];
+                for (let y = 0, height = color.height; y < height; y++) {
+                    for (let x = 0, width = color.width; x < width; x++) {
+                        let i = (y * 4) * color.width + x * 4;
+                        let pixelVal = parseInt((pixels.data[i] + pixels.data[i + 1] + pixels.data[i + 2]) / 3, 10);
+                        mono.push(pixelVal);
+                    }
+                }
+                
+                let height = mono.length / BMP_BYTES_PER_LINE;
+                let n1 = (height & 0xff00) >>> 8;
+                let n2 = (height & 0x00ff);
+
+                // プリンタに送信
+                printer.write( Buffer.concat([header, n1, n2]), DEFAULT_TIMEOUT);
+                for (let from = 0, len = mono.length; from < len; from += MAX_USBFS_BUFFER_SIZE) {
+                    let to = Math.min(mono.length, from + MAX_USBFS_BUFFER_SIZE);
+                    printer.write(mono.slice(from, to));
+                }
+            });
+        })
+    }
+
+    drawLogo(ctx, aImage, aOffset, isRender) {
+        let posX = (RECEIPT_WIDTH / 2) - (aImage.width / 2);
+        aOffset.y += 48;    // 上マージン
+        if (isRender) {
+            ctx.drawImage(aImage, posX, aOffset.y, aImage.width, aImage.height); // 画像配置
+        }
+        aOffset.y += aImage.height; // 画像高さをオフセットに加算
+        aOffset.y += 60; 
+    }
+
+    drawResultText(ctx, aMsg, aOffset, isRender) {
+        ctx.font = "bold 34px";
+
+        aMsg = aMsg.replace('\n', '');
+        msgArr = [];
+        for (let cnt = 0, len = aMsg.length/ 10; cnt < len; cnt++ ) {
+            this.drawTextCenter(ctx, aMsg.substr(cnt * 10, 10), 34, 24,  aOffset, isRender);
+        }
+        aOffset.y += 45;    // 下マージン
+    }
+
+    drawMessage(ctx, aOffset, isRender) {
+        ctx.font = "bold 22px";
+
+        let index = Math.floor( Math.random() * MESSAGE_ARRAY.length );
+
+        this.drawTextCenter(ctx, MESSAGE_ARRAY[index], 22, 0, aOffset, isRender);
+        
+        aOffset.y += 60;
+    }
+
+    drawHeading(ctx, aImage, aOffset, isRender) {
+        if (isRender) {
+            ctx.drawImage(aImage, 0, aOffset.y, aImage.width, aImage.height);   // 画像配置
+        }
+        aOffset.y += aImage.height;
+        aOffset.y += 20;    // 下マージン
+    }
+
+    drawTextCenter(ctx, aMessage, aFontSize, aLineSpace, aOffset, isRender) {
+        let metrics = ctx.measureText(aMessage);
+        let textWidth = metrics.width;
+        let offsetX = (RECEIPT_WIDTH / 2) - (textWidth / 2);
+        aOffsetY += aFontSize + aLineSpace;
+        if (isRender) {
+            ctx.fillText(aMessage, offsetX, aOffset.y);
+        }
+    }
+}
+
+const generator = new ReceiptGenerater();
+
+const app = express();
+app.use(multer().none());
+app.use(jexpress.static('web'));
+
+app.post('/api/generate', (req, res) => {
+    const data = req.body;
+    const text = data.text;
+    console.log(text);
+    generator.generateImage(text).then(() => {
+
+    });
+    res.json({msg: 'OK'});
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
